2c086d5aba0849110f3cb50cfa57ac15d037d07b
Yi HAN <hanyi@8110@gmail.com>
2020-06-07 23:02:37 +0200

    ejs template engine

--- server.js
+++ server.js
@@ -5,14 +5,20 @@ const app = express();
 
 const port = 3000;
 
-app.get("/", (req, res) => {
-    res.sendfile(path.join(__dirname, 'static', 'index.html'));
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.static(path.join(__dirname, 'static')));
+
+app.get('/', (req, res) => {
+//   res.sendfile(path.join(__dirname, 'static', 'index.html'));
+    res.render('pages/index', {pageTitle: "Hello world"});
 });
 
-app.get("/speakers", (req, res) => {
-    res.sendfile(path.join(__dirname, 'static', 'speakers.html'));
+app.get('/speakers', (req, res) => {
+  res.sendfile(path.join(__dirname, 'static', 'speakers.html'));
 });
 
 app.listen(port, () => {
-    console.log(`Listen on port ${port}`);
+  console.log(`Listen on port ${port}`);
 });
