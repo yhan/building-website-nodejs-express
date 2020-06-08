4394f15b0db106ff2714a225cb1ed75e1369db59
Yi HAN <hanyi@8110@gmail.com>
2020-06-08 12:47:03 +0200

    session cookie

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,26 @@
 const express = require('express');
-const speakerRouter = require('./speakers');
-const feedbackRouter = require('./feedback');
+const speakersRoute = require('./speakers');
+const feedbackRoute = require('./feedback');
 
 const router = express.Router();
 
 module.exports = (speakerService, feedbackService) => {
-  router.get('/', (req, res) => {
-    res.render('pages/index', { pageTitle: 'Hello world' });
+  router.get('/', (request, response) => {
+
+    // dev test: session is per user
+    // effect of use cookie session in `server.js`
+    if (!request.session.myCounter) {
+      request.session.myCounter = 0;
+    }
+    request.session.myCounter += 1;
+    console.log(`Nb of visit: ${request.session.myCounter}`);
+
+    response.render('pages/index', { pageTitle: 'Welcome' });
+    
   });
 
-  router.use('/speakers', speakerRouter(speakerService));
-  router.use('/feedback', feedbackRouter(feedbackService));
+  router.use('/speakers', speakersRoute(speakerService));
+  router.use('/feedback', feedbackRoute(feedbackService));
+
   return router;
 };
--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const path = require('path');
+const cookieSession = require('cookie-session');
+
 const routers = require('./routes/index'); // default to index.js
 const SpeakerService = require('./services/SpeakerService');
 const FeedbackService = require('./services/FeedbackService');
@@ -14,15 +16,18 @@ const port = 3000;
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+app.set('trust proxy', 1);
+app.use(
+  cookieSession({
+    name: 'session',
+    keys: ['dfmjqdsm', '0UJHHdfkd'],
+  })
+);
+
 app.use(express.static(path.join(__dirname, 'static')));
 
-app.use(
-  '/',
-  routers(
-    speakerService,
-    feedbackService,
-  )
-);
+app.use('/', routers(speakerService, feedbackService));
+
 
 app.listen(port, () => {
   console.log(`Listen on port ${port}`);
